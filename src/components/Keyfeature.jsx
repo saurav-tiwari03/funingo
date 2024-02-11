e4b2bb3d33f94ef29b6bd482360fa4029d159be3
Saurav Tiwari <116860218+saurav-tiwari03@users.noreply.github.com>
2024-02-11 17:11:06 +0530

    changed background images

--- src/components/Keyfeature.jsx
+++ src/components/Keyfeature.jsx
@@ -8,7 +8,7 @@ export default function Keyfeature() {
         <h2 className='my-4 bg-[#416133] py-4 px-8 text-4xl text-white font-bold'>Key Features</h2>
       </div>
       <div class="main-container">
-        <div class="item1"></div>
+        <div class="item1 one"></div>
         <div class="item2 pop-hover">
             <div id="content-layout">
                 <h1 className='text-[#416133] font-bold text-3xl'>Immersive Battle Arenas:</h1>
@@ -23,7 +23,7 @@ export default function Keyfeature() {
         </div>
       </div>
       <div class="main-container">
-        <div class="item1"></div>
+        <div class="item1 two"></div>
         <div class="item2 pop-hover " id='even'>
             <div id="content-layout">
                 <h1 className='text-[#416133] font-bold text-3xl'>High-Quality Paintball Gear</h1>
@@ -36,7 +36,7 @@ enhancing the intensity of every skirmish</p>
         </div>
       </div>
       <div class="main-container">
-        <div class="item1"></div>
+        <div class="item1 three"></div>
         <div class="item2 pop-hover">
             <div id="content-layout">
                 <h1 className='text-[#416133] font-bold text-3xl'>Biodegradable Paintballs:</h1>
@@ -49,7 +49,7 @@ adventure is environmentally responsible. </p>
         </div>
       </div>
       <div class="main-container">
-        <div class="item1"></div>
+        <div class="item1 four"></div>
         <div class="item2 pop-hover" id='even'>
             <div id="content-layout">
                 <h1 className='text-[#416133] font-bold text-3xl'>Strategic Gameplay:</h1>
@@ -60,7 +60,7 @@ adventure is environmentally responsible. </p>
         </div>
       </div>
       <div class="main-container">
-        <div class="item1"></div>
+        <div class="item1 five"></div>
         <div class="item2 pop-hover">
             <div id="content-layout">
                 <h1 className='text-[#416133] font-bold text-3xl'> Safety-First Approach:</h1>
@@ -71,7 +71,7 @@ masks, goggles, and padded clothing</p>
         </div>
       </div>
       <div class="main-container">
-        <div class="item1"></div>
+        <div class="item1 six"></div>
         <div class="item2 pop-hover" id='even'>
             <div id="content-layout">
                 <h1 className='text-[#416133] font-bold text-3xl'> Team Building and Camaraderie</h1>
